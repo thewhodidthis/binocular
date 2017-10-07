d11c0026a7f81b65bd3ee2d63fbfbe476ad2a773
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-07 21:55:03 +0200

    module: cleanup graph helpers

--- graph.js
+++ graph.js
@@ -1,74 +1,73 @@
 export const TAU = Math.PI * 2
-export const deg = TAU / 360
 
-export const linear = (source, target, adjust) => {
-  const count = source.frequencyBinCount
-  const { width: w, height: h } = target.canvas
+export const linearly = (context) => {
+  const { width: w, height: h } = context.canvas
 
   // Vertical center
   const halfH = h * 0.5
 
-  // Diameter, available space
-  const d = Math.min(w, h)
-
-  // Radius
-  const r = d * 0.5
-
-  // Horizontal step multiplier
-  const s = Math.round(w / count)
-
-  return (values) => {
-    target.clearRect(0, 0, w, h)
-
-    target.save()
-    target.translate(0, halfH)
-    target.beginPath()
-
-    for (let i = 0; i < count; i += 1) {
-      const v = values[i]
-
-      // Make sure a pixel is drawn when zero, doesn't look very nice otherwise
-      const y = r * adjust(v) || 1
-      const x = i * s
-
-      target.moveTo(x, y)
-      target.lineTo(x, y * -1)
-      target.stroke()
-    }
-
-    target.restore()
-  }
-}
-
-export const radial = (source, target, adjust) => {
-  const count = source.frequencyBinCount
-  const { width: w, height: h } = target.canvas
-
-  const steps = 360 / count
-  const halfH = h * 0.5
-  const halfW = w * 0.5
-
-  // Figure out available space
+  // Available space
   const d = Math.min(w, h)
 
   // Base radius
-  const r = d * 0.325
+  const r = d * 0.5
 
-  // Precalculate multiplier
-  const f = r * 0.5
+  return (values, weight) => {
+    const bins = values.length
+    const step = Math.round(w / bins)
 
-  return (values) => {
-    target.clearRect(0, 0, w, h)
+    context.clearRect(0, 0, w, h)
 
-    target.save()
-    target.translate(halfW, halfH)
-    target.rotate(-0.25 * TAU)
+    context.save()
+    context.translate(0, halfH)
+    context.beginPath()
 
-    for (let i = 0; i < count; i += 1) {
-      const angle = i * steps * deg
+    for (let i = 0; i < bins; i += 1) {
+      const v = values[i]
+
+      // Make sure a pixel is drawn when zero, doesn't look very nice otherwise
+      const y = r * weight(v) || 1
+      const x = i * step
+
+      context.moveTo(x, y)
+      context.lineTo(x, y * -1)
+      context.stroke()
+    }
+
+    context.restore()
+  }
+}
+
+export const radially = (context) => {
+  const { width: w, height: h } = context.canvas
+
+  const halfH = h * 0.5
+  const halfW = w * 0.5
+
+  // Available space
+  const d = Math.min(w, h) * 0.75
+
+  // Base radius
+  const r = d * 0.5
+
+  // Radial multiplier
+  const f = r * 0.25
+
+  return (values, weight) => {
+    const bins = values.length
+    const step = TAU / bins
+
+    context.clearRect(0, 0, w, h)
+
+    context.save()
+    context.translate(halfW, halfH)
+    context.rotate(-0.25 * TAU)
+
+    for (let i = 0; i < bins; i += 1) {
+      const angle = i * step
 
       const v = values[i]
-      const k = f * adjust(v) || 1
+      const k = f * weight(v) || 1
 
       const r1 = r - k
       const r2 = r + k
@@ -77,12 +76,12 @@ export const radial = (source, target, adjust) => {
       const x2 = r2 * Math.cos(angle)
       const y2 = r2 * Math.sin(angle)
 
-      target.beginPath()
-      target.moveTo(x1, y1)
-      target.lineTo(x2, y2)
-      target.stroke()
+      context.beginPath()
+      context.moveTo(x1, y1)
+      context.lineTo(x2, y2)
+      context.stroke()
     }
 
-    target.restore()
+    context.restore()
   }
 }
